bd6988e58eb70ff9d17532f5409c766eabce460f
Jeff Bowles <jeff@thebowles.ca>
2019-02-11 18:30:38 -0800

    Update install.sh

--- nginx/install.sh
+++ nginx/install.sh
@@ -3,6 +3,6 @@
 # This will gather a list of all paths in the current directory, excluding *.sh files
 # and then copy recursively to the nginx conf dir.
 
-echo -e "Copying NGINX configuration files to /etc/nginx/conf ..."
+echo -e "Copying NGINX configuration files to /usr/local/nginx/conf ..."
 ls | grep -v *.sh | xargs -I{} sudo cp -rf "{}" /usr/local/nginx/conf
 echo -e "Copy complete, have a nice day."
